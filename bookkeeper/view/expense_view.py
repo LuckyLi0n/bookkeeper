ad8c062f8220f1f0cd89e2452b6288c4e8ce8d55
LuckyLi0n <Rita8996@mail.ru>
2023-03-05 22:06:20 +0300

    expense_view.py is created

---
+++ bookkeeper/view/expense_view.py
@@ -0,0 +1,78 @@
+from PySide6 import QtCore, QtWidgets
+from PySide6.QtWidgets import QVBoxLayout, QLabel, QWidget, QGridLayout, QComboBox, QLineEdit, QPushButton
+
+
+class TableModel(QtCore.QAbstractTableModel):
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == QtCore.Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        return len(self._data)
+
+    def columnCount(self, index):
+        return len(self._data[0])
+
+
+class ExpenseWidget(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.item_model = None
+
+        self.layout = QVBoxLayout()
+
+        self.layout.addWidget(QLabel('Последние расходы'))
+
+        self.expenses_grid = QtWidgets.QTableView()
+        self.layout.addWidget(self.expenses_grid)
+
+        self.bottom_controls = QGridLayout()
+
+        self.bottom_controls.addWidget(QLabel('Сумма'), 0, 0)
+
+        self.amount_line_edit = QLineEdit()
+
+        self.bottom_controls.addWidget(self.amount_line_edit, 0, 1)
+        self.bottom_controls.addWidget(QLabel('Категория'), 1, 0)
+
+        self.category_dropdown = QComboBox()
+
+        self.bottom_controls.addWidget(self.category_dropdown, 1, 1)
+
+        self.category_edit_button = QPushButton('Редактировать')
+        self.bottom_controls.addWidget(self.category_edit_button, 1, 2)
+
+        self.expense_add_button = QPushButton('Добавить')
+        self.bottom_controls.addWidget(self.expense_add_button, 2, 1)
+
+        self.bottom_widget = QWidget()
+        self.bottom_widget.setLayout(self.bottom_controls)
+
+        self.layout.addWidget(self.bottom_widget)
+
+        self.widget = QWidget()
+        self.widget.setLayout(self.layout)
+
+        self.setCentralWidget(self.widget)
+
+    def set_expense_table(self, data):
+        self.item_model = TableModel(data)
+        self.expenses_grid.setModel(self.item_model)
+
+    def set_category_dropdown(self, data):
+        for tup in data:
+            self.category_dropdown.addItem(tup[1], tup[0])
+
+    def on_expense_add_button_clicked(self, slot):
+        self.expense_add_button.clicked.connect(slot)
+
+    def get_amount(self) -> float:
+        return float(self.amount_line_edit.text())  # TODO: обработка исключений
+
+    def get_selected_cat(self) -> int:
+        return self.category_dropdown.itemData(self.category_dropdown.currentIndex())
