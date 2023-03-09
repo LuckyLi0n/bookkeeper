13b0b1a7bc63ac0c968ce0eefbc1d759e2f06b60
LuckyLi0n <Rita8996@mail.ru>
2023-03-09 13:22:07 +0300

    add comment to expense_view.py

--- bookkeeper/view/expense_view.py
+++ bookkeeper/view/expense_view.py
@@ -1,5 +1,6 @@
 from PySide6 import QtWidgets
-from PySide6.QtWidgets import QVBoxLayout, QLabel, QWidget, QGridLayout, QComboBox, QLineEdit, QPushButton
+from PySide6.QtWidgets import QVBoxLayout, QLabel, QWidget,\
+    QGridLayout, QComboBox, QLineEdit, QPushButton
 
 from bookkeeper.view.Table_model import TableModel
 
@@ -20,21 +21,22 @@ class ExpenseWidget(QtWidgets.QMainWindow):
         self.bottom_controls = QGridLayout()
 
         self.bottom_controls.addWidget(QLabel('Сумма'), 0, 0)
-
         self.amount_line_edit = QLineEdit()
-
         self.bottom_controls.addWidget(self.amount_line_edit, 0, 1)
+
         self.bottom_controls.addWidget(QLabel('Категория'), 1, 0)
-
         self.category_dropdown = QComboBox()
-
         self.bottom_controls.addWidget(self.category_dropdown, 1, 1)
+
+        self.bottom_controls.addWidget(QLabel('Комментарий'), 2, 0)
+        self.comment_line_edit = QLineEdit()
+        self.bottom_controls.addWidget(self.comment_line_edit, 2, 1)
 
         self.category_edit_button = QPushButton('Редактировать')
         self.bottom_controls.addWidget(self.category_edit_button, 1, 2)
 
         self.expense_add_button = QPushButton('Добавить')
-        self.bottom_controls.addWidget(self.expense_add_button, 2, 1)
+        self.bottom_controls.addWidget(self.expense_add_button, 3, 1)
 
         self.bottom_widget = QWidget()
         self.bottom_widget.setLayout(self.bottom_controls)
@@ -59,7 +61,10 @@ class ExpenseWidget(QtWidgets.QMainWindow):
         self.expense_add_button.clicked.connect(slot)
 
     def get_amount(self) -> float:
-        return float(self.amount_line_edit.text())  # TODO: обработка исключений
+        return float(self.amount_line_edit.text())
+
+    def get_comment(self) -> str:
+        return str(self.comment_line_edit.text())
 
     def get_selected_cat(self) -> int:
         return self.category_dropdown.itemData(self.category_dropdown.currentIndex())
